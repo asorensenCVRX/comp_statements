69dfd9d7032d9342b5634c9eb957b9c6f0dd2932
asorensen <asorensen@cvrx.com>
2024-06-18 13:02:33 -0600

    Updated database connection string, no longer requires MFA each time you run the script

--- database.py
+++ database.py
@@ -1,15 +1,25 @@
 import pandas as pd
 from sqlalchemy.engine import URL, create_engine
 from VARIABLES import comp_mm
+from azure.identity import DefaultAzureCredential
+import struct
 
 # connection parameters
-server = 'ods-sql-server-us.database.windows.net'
+server = 'tcp:ods-sql-server-us.database.windows.net'
 database = 'salesops-sql-prod-us'
-username = 'asorensen@cvrx.com'
-conn_str = (f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={server};DATABASE={database};UID={username};'
-            f'Authentication=ActiveDirectoryInteractive')
-connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": conn_str})
-engine = create_engine(connection_url)
+conn_str = (f'DRIVER=ODBC Driver 17 for SQL Server;'
+            f'SERVER={server};'
+            f'DATABASE={database};'
+            f'ENCRYPT=yes;'
+            f'TRUSTSERVERCERTIFICATE=no;'
+            f'connection timeout=30')
+credential = DefaultAzureCredential(exclude_interactive_browser_credential=False)
+token = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
+token_struct = struct.pack(f'<I{len(token)}s', len(token), token)
+connection_url = URL.create("mssql+pyodbc",
+                            query={"odbc_connect": conn_str})
+engine = create_engine(connection_url,
+                       connect_args={"attrs_before": {1256: token_struct}})
 
 
 def get_queries(conn):

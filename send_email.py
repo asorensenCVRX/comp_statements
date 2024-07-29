b07778762da6e5422e106fd9e56bec3fcae588c1
asorensen <asorensen@cvrx.com>
2024-07-29 12:11:33 -0600

    add error handling for send_email module

--- send_email.py
+++ send_email.py
@@ -9,64 +9,76 @@ def send_am_email(payees, month_mm: str, month_name: str, is_prelim: bool):
     print("Sending AM prelim emails...") if is_prelim \
         else print("Sending AM official emails...")
     for key, value in payees.am_info.items():
-        folder = am_prelim_directory if is_prelim \
-            else am_directory
-        file_name = f'PRELIMINARY_{key}_2024_{month_mm}.pdf' if is_prelim \
-            else f'{key}_2024_{month_mm}.pdf'
-        path = os.path.join(folder, file_name)
-        subject = f"PRELIMINARY {month_name} Comp Statement: {value['TERR_NM']}" if is_prelim \
-            else f"{month_name} Comp Statement: {value['TERR_NM']}"
-        # Check if rep reports to a TM; if so, add the rep's TM and RM as managers so both get CC'd
-        manager_email = None
-        for tm, rep in payees.tm_reports.items():
-            if value["EMAIL"] in rep:
-                manager_email = f"{value['RM_EMAIL']}; {tm}"
-                break
-            else:
-                manager_email = value['RM_EMAIL']
-        template = am_prelim_email if is_prelim \
-            else am_official_email
-        email = SendEmail(template=template, recipient_fullname=key, recipient_first_name=value['FNAME_REP'],
-                          recipient_email=value['EMAIL'], manager_email=manager_email, subject=subject,
-                          attachment_path=path)
-        email.send_email()
+        try:
+            folder = am_prelim_directory if is_prelim \
+                else am_directory
+            file_name = f'PRELIMINARY_{key}_2024_{month_mm}.pdf' if is_prelim \
+                else f'{key}_2024_{month_mm}.pdf'
+            path = os.path.join(folder, file_name)
+            subject = f"PRELIMINARY {month_name} Comp Statement: {value['TERR_NM']}" if is_prelim \
+                else f"{month_name} Comp Statement: {value['TERR_NM']}"
+            # Check if rep reports to a TM; if so, add the rep's TM and RM as managers so both get CC'd
+            manager_email = None
+            for tm, rep in payees.tm_reports.items():
+                if value["EMAIL"] in rep:
+                    manager_email = f"{value['RM_EMAIL']}; {tm}"
+                    break
+                else:
+                    manager_email = value['RM_EMAIL']
+            template = am_prelim_email if is_prelim \
+                else am_official_email
+            email = SendEmail(template=template, recipient_fullname=key, recipient_first_name=value['FNAME_REP'],
+                              recipient_email=value['EMAIL'], manager_email=manager_email, subject=subject,
+                              attachment_path=path)
+            email.send_email()
+        except Exception as e:
+            print(f"There was an error {e}.\nUnable to send email to {key}: {value}")
+            continue
 
 
 def send_rm_email(payees, month_mm: str, month_name: str, is_prelim: bool):
     print("Sending RM prelim emails...") if is_prelim \
         else print("Sending RM official emails...")
     for key, value in payees.rm_info.items():
-        folder = rm_prelim_directory if is_prelim \
-            else rm_directory
-        file_name = f'PRELIMINARY_{key}_2024_{month_mm}.pdf' if is_prelim \
-            else f"{month_name} Comp Statement: {value['REGION']}"
-        path = os.path.join(folder, file_name)
-        subject = f"PRELIMINARY {month_name} Comp Statement: {value['REGION']}" if is_prelim \
-            else f"{month_name} Comp Statement: {value['REGION']}"
-        template = rm_prelim_email if is_prelim \
-            else rm_official_email
-        email = SendEmail(template=template, recipient_fullname=key, recipient_first_name=value['FNAME'],
-                          recipient_email=value['EMAIL'], manager_email=None, subject=subject, attachment_path=path)
-        email.send_email()
+        try:
+            folder = rm_prelim_directory if is_prelim \
+                else rm_directory
+            file_name = f'PRELIMINARY_{key}_2024_{month_mm}.pdf' if is_prelim \
+                else f"{key}_2024_{month_mm}.pdf"
+            path = os.path.join(folder, file_name)
+            subject = f"PRELIMINARY {month_name} Comp Statement: {value['REGION']}" if is_prelim \
+                else f"{month_name} Comp Statement: {value['REGION']}"
+            template = rm_prelim_email if is_prelim \
+                else rm_official_email
+            email = SendEmail(template=template, recipient_fullname=key, recipient_first_name=value['FNAME'],
+                              recipient_email=value['EMAIL'], manager_email=None, subject=subject, attachment_path=path)
+            email.send_email()
+        except Exception as e:
+            print(f"There was an error {e}.\nUnable to send email to {key}: {value}")
+            continue
 
 
 def send_csr_email(payees, month_mm: str, month_name: str, is_prelim: bool):
     print("Sending CSR prelim emails...") if is_prelim \
         else print("Sending CSR official emails...")
     for key, value in payees.csr_info.items():
-        folder = csr_prelim_directory if is_prelim \
-            else csr_directory
-        file_name = f'PRELIMINARY_{key}_2024_{month_mm}.pdf' if is_prelim \
-            else f'{key}_2024_{month_mm}.pdf'
-        path = os.path.join(folder, file_name)
-        subject = f"PRELIMINARY {month_name} Comp Statement: {value['TERR_NM']}" if is_prelim \
-            else f"{month_name} Comp Statement: {value['TERR_NM']}"
-        template = am_prelim_email if is_prelim \
-            else am_official_email
-        email = SendEmail(template=template, recipient_fullname=key, recipient_first_name=value['FNAME_REP'],
-                          recipient_email=value['EMAIL'], manager_email=value['RM_EMAIL'], subject=subject,
-                          attachment_path=path)
-        email.send_email()
+        try:
+            folder = csr_prelim_directory if is_prelim \
+                else csr_directory
+            file_name = f'PRELIMINARY_{key}_2024_{month_mm}.pdf' if is_prelim \
+                else f'{key}_2024_{month_mm}.pdf'
+            path = os.path.join(folder, file_name)
+            subject = f"PRELIMINARY {month_name} Comp Statement: {value['TERR_NM']}" if is_prelim \
+                else f"{month_name} Comp Statement: {value['TERR_NM']}"
+            template = am_prelim_email if is_prelim \
+                else am_official_email
+            email = SendEmail(template=template, recipient_fullname=key, recipient_first_name=value['FNAME_REP'],
+                              recipient_email=value['EMAIL'], manager_email=value['RM_EMAIL'], subject=subject,
+                              attachment_path=path)
+            email.send_email()
+        except Exception as e:
+            print(f"There was an error {e}.\nUnable to send email to {key}: {value}")
+            continue
 
 
 class SendEmail:
